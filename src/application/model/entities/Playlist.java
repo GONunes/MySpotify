d98ba761b339217f4bdfa3857f4632a7ba95cc5b
sidney S.M <sidneysilvantl99@gmail.com>
2020-05-15 17:53:52 -0300

    Adicionado gets e sets, playlist e construtores da mesma

--- src/application/model/entities/Playlist.java
+++ src/application/model/entities/Playlist.java
@@ -1,5 +1,33 @@
 package application.model.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
+	
 	private String nome;
+	private List<Song> songs = new ArrayList<>();
+	
+	public Playlist() {}
+	
+	public Playlist(String nome, List<Song> songs) {
+		this.nome = nome;
+		this.songs = songs;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public List<Song> getSongs() {
+		return songs;
+	}
+	public void setSongs(List<Song> songs) {
+		this.songs = songs;
+	}
+	
+	
+		
 }
