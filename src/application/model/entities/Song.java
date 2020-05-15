a4dae0345dde509ad626113c2813785e44591771
sidney S.M <sidneysilvantl99@gmail.com>
2020-05-15 17:30:17 -0300

    Mudar o nome da classe Music para Song

--- src/application/model/entities/Song.java
+++ src/application/model/entities/Song.java
@@ -1,18 +1,19 @@
 package application.model.entities;
 
-public class Music {
+public class Song {
 	String titulo;
 	int ano;
 	String genero;
 	double duracao;
 	Composer compositor;
 	
-	public void exibir() {
-		System.out.println("Título da Música: " + titulo + "\n" +
+	@Override
+	public String toString() {
+		return "Título da Música: " + titulo + "\n" +
 							"Ano: " + ano + "\n" +
 							"Gênero: " + genero + "\n" +
 							"Duração: " + duracao + "\n" +
-							"Compositor: " + compositor);
+							"Compositor: " + compositor;
 	}
 
 }
