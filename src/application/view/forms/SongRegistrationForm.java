6fd31a15e58b6d4ec4950e621b96a4c481245568
Guilherme Nunes <deoliveiranunes.guilherme@hotmail.com>
2020-05-16 17:19:33 -0300

    Ajustando Formulario de Cadastro de Musica

--- src/application/view/forms/SongRegistrationForm.java
+++ src/application/view/forms/SongRegistrationForm.java
@@ -13,33 +13,41 @@ public class SongRegistrationForm {
 		Song song = new Song();
 		Scanner sc = new Scanner(System.in);
 		
+		Screen.clear();
+		
 		System.out.println(" Digite o título da musica: ");
 		System.out.print(" > ");
 		String title = sc.nextLine();
 		System.out.print("\n");
 		song.setTitulo(title);
+		
 		System.out.println(" Digite o ano da musica: ");
 		System.out.print(" > ");
 		int year = sc.nextInt();
 		System.out.print("\n");
 		song.setAno(year);
+		
 		System.out.println(" Digite o genêro da musica: ");
 		System.out.print(" > ");
-		String genre = sc.nextLine();
+		String genre = sc.next();
 		System.out.print("\n");
 		song.setGenero(genre);
+		
 		System.out.println(" Digite a duração da musica: ");
 		System.out.print(" > ");
 		double duration = sc.nextDouble();
 		System.out.print("\n");
 		song.setDuracao(duration);
+		
 		System.out.println(" Digite o nome do compositor: ");
 		System.out.print(" > ");
-		Composer songwriter = new Composer();
+		String songwriter = sc.next();
+		Composer composer = new Composer();
 		System.out.print("\n");
-		song.setCompositor(songwriter);
+		composer.setName(songwriter);
 		
-		//SongRepository.add(song);
+		SongRepository.add(song);
+		System.out.print("Música cadastrada com sucesso!");
 		
 	}
 
