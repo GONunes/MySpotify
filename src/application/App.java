360041b2c3e4d831968a7196e6e5606d73d742a5
Dilermando A. H <dilermandohelcias@gmail.com>
2020-05-16 17:20:19 -0300

    Melhorar a função de adicionar música

--- src/application/App.java
+++ src/application/App.java
@@ -1,13 +1,24 @@
 package application;
 
 import application.config.DatabaseConfig;
+import application.model.entities.Composer;
+import application.model.entities.Song;
+import application.model.repositories.SongRepository;
 import application.view.Console;
 import application.view.screen.HomeUserScreen;
 
 public class App {
 	public static void main(String[] args) {
 		DatabaseConfig.started();
-		Console.start();
-		HomeUserScreen.view();
+//		Console.start();
+//		HomeUserScreen.view();
+		
+		
+		Song s = new Song("Girls of Summer", 1980, "Rock", 3.80, new Composer("Aerosmith"));
+		Song st = new Song("Could Have Been Me", 2016, "Rock", 3.80, new Composer("The Struts"));
+		
+		SongRepository.add(s);
+		SongRepository.add(st);
+		
 	}
 }
