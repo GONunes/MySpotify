aed284c72530c79065c61411174941a4659a8a9b
Dilermando A. H <dilermandohelcias@gmail.com>
2020-05-16 12:39:35 -0300

    Polir telas de login e cadastro de usuário

--- src/application/view/screen/LoginScreen.java
+++ src/application/view/screen/LoginScreen.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 import application.auth.UserAuth;
 import application.config.UserConfig;
 import application.model.entities.User;
+import application.util.Screen;
 import application.view.forms.UserRegistrationForm;
 
 public class LoginScreen {
 	public static void view() {
 		String username = "";
 		Scanner sc = new Scanner(System.in);
+		boolean err = false;
 		
 		while(true) {
-			System.out.println("Informe seu usuário:");
+			Screen.clear();
+			
+			if(err)
+				System.out.println("Usuário informado está incorreto!");
+			
+			System.out.println("Informe seu usuário" + (err ? " correto:" : ":") );
 			System.out.println("Caso não tenha um usuário, digite 0 para se cadastrar!");
-			System.out.print(" > ");
+			System.out.print("> ");
 			username = sc.nextLine();
 			
 			if(username.equals("0"))
@@ -25,11 +32,9 @@ public class LoginScreen {
 				
 				if (u != null) {
 					UserConfig.setUser(u);
-				} else {
-					System.out.println("Infelizmente o usuário informado está incorreto");
-					System.out.println("Precione qualquer tecla para continuar...");
-					sc.nextLine();
-				}
+					HomeUserScreen.view();
+				} else
+					err = true;
 				
 			}
 			
