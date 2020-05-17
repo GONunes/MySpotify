75f681cc9e187dd15bb0e479527c7270d52f6b43
Dilermando A. H <dilermandohelcias@gmail.com>
2020-05-16 21:29:59 -0300

    Vincular a playlist criada ao seu criador

--- src/application/model/repositories/PlaylistRepository.java
+++ src/application/model/repositories/PlaylistRepository.java
@@ -3,21 +3,22 @@ package application.model.repositories;
 import java.sql.PreparedStatement;
 
 import application.config.DatabaseConfig;
+import application.config.UserConfig;
 import application.model.entities.Playlist;
 import application.model.entities.Song;
 
 public class PlaylistRepository {
 	
 	public static void add(Playlist playlist) {
-		String sqlPlaylist = "INSERT INTO playlists (nome) VALUES (?) ";
+		String sqlPlaylist = "INSERT INTO playlists (createdBy, nome) VALUES (?,?) ";
 		String sqlPlaylistSong = "INSERT INTO playlists_songs (playlist_id, song_id) VALUES (?,?)";
 		int idPlaylist = 0;
 		
 		try {
 			PreparedStatement psp = DatabaseConfig.getConnect()
 												.prepareStatement(sqlPlaylist);
-			
-			psp.setString(1, playlist.getNome());
+			psp.setInt(1, UserConfig.getUser().getId());
+			psp.setString(2, playlist.getNome());
 			
 			psp.executeUpdate();
 			idPlaylist = DatabaseConfig.getConnect()
