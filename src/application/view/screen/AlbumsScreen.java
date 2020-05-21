7c95aef94ce2a4941abc891e1861e7060a5e6866
sidney S.M <sidneysilvantl99@gmail.com>
2020-05-21 20:23:41 -0300

    mostrar compositor com erro

--- src/application/view/screen/AlbumsScreen.java
+++ src/application/view/screen/AlbumsScreen.java
@@ -1,7 +1,40 @@
 package application.view.screen;
 
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+import application.model.entities.Album;
+import application.model.repositories.AlbumRepository;
+import application.util.Screen;
+
 public class AlbumsScreen {
-	public static void view () {
+public static void view() {
 		
+		Screen.clear();
+		Scanner sc = new Scanner(System.in);
+	
+		 List<Album> albuns = AlbumRepository.getAll();
+		 
+		 if(albuns == null) {
+				System.out.println("Nenhum album registrado");	
+				System.out.println();
+				System.out.println("Pressione Enter para retornar ao menu principal.");
+				sc.nextLine();
+		 } else {
+			 ListIterator<Album> it = albuns.listIterator();
+			 System.out.println("Albuns: ");
+			 System.out.println();
+			 
+			 while(it.hasNext()) {
+				for(int i = 0; i < albuns.size(); i++) {
+					System.out.println(it.next().toString());
+					System.out.println();
+				 }
+			 	
+			 	System.out.println("Pressione Enter para Continuar.");
+				sc.nextLine();
+			 }
+		 }
 	}
 }
